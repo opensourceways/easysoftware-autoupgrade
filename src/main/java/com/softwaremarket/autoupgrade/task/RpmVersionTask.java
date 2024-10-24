76af274ca209c2abb4fc2b0b2a08d102aadbf7c6
ZWJason <zwjsec@mail.ustc.edu.cn>
2024-10-24 14:50:10 +0800

    [feat] add refresh remote repo

--- src/main/java/com/softwaremarket/autoupgrade/task/RpmVersionTask.java
+++ src/main/java/com/softwaremarket/autoupgrade/task/RpmVersionTask.java
@@ -1,25 +1,69 @@
+/* Copyright (c) 2024 openEuler Community
+ EasySoftwareInput is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ See the Mulan PSL v2 for more details.
+*/
 package com.softwaremarket.autoupgrade.task;
 
 import com.alibaba.fastjson.JSONObject;
+import com.softwaremarket.autoupgrade.config.RepoConfig;
 import com.softwaremarket.autoupgrade.handler.RpmUpdateHandler;
 import com.softwaremarket.autoupgrade.helper.EasysoftwareVersionHelper;
+import com.softwaremarket.autoupgrade.service.impl.GitService;
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.List;
 
-@Slf4j
 @Service
 @EnableAsync
 @RequiredArgsConstructor
 public class RpmVersionTask {
 
+    /**
+     * logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpmVersionTask.class);
+
+    /**
+     * easysoftwareVersionHelper.
+     */
     private final EasysoftwareVersionHelper easysoftwareVersionHelper;
+
+    /**
+     * rpmUpdateHandler.
+     */
     private final RpmUpdateHandler rpmUpdateHandler;
 
+    /**
+     * git service.
+     */
+    @Autowired
+    GitService gitSvc;
+
+    /**
+     * config of repo.
+     */
+    @Autowired
+    RepoConfig repoConfig;
+
+    /**
+     * auto upgrade rpm version.
+     *
+     */
     //@Scheduled(cron = "${softwareconfig.rpmSchedule}")
     public void rpmAutocommit() {
         //  Set<String> rpmNameSet = giteeService.getReposProjects(rpmConfig.getRepo(), forkConfig.getAccessToken());
@@ -37,8 +81,21 @@ public class RpmVersionTask {
                 e.printStackTrace();
             }
         }
-
     }
 
+    /**
+     * refresh remote upstream repo by using git clone or git pull.
+     *
+     */
+    public void refreshUpstreamRepo() {
+
+        List<String> remotePathList = repoConfig.getRepolist();
+
+        for (String remotePath : remotePathList) {
+            gitSvc.cloneOrPull(remotePath);
+        }
+
+        LOGGER.info("refresh upstream end");
+    }
 
 }
